2b4440048f1fa0e23e0af30a55120da47cf351e6
alfaqih94 <abdullahfaqih.1994@gmail.com>
2026-01-27 01:43:37 +0700

    SI Akad V-27 -

--- script/config.js
+++ script/config.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://script.google.com/macros/s/AKfycbxr69SgfU5CAHxaFcril-x9iRQg66CqA3_JEtLMRIkdHIztKbAb5SmeilmHM-Byf1pu/exec";
+  "https://script.google.com/macros/s/AKfycby65rTK2ZLrVeN_zv0WyAxJV5jhk7VEbu2qQfQok7lo70ShBVbGGGhkhcdZ8T3Apz1M/exec";
